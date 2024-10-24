c0b350b3ff60a14684288992b1e4aea2c098589d
liangzid <2273067585@qq.com>
2024-10-24 22:36:59 +0800

    update the instruction and the main inference file.

---
+++ 1.run_vanilla_construct.py
@@ -0,0 +1,59 @@
+"""
+======================================================================
+1.RUN_VANILLA_CONSTRUCT ---
+
+Vanilla version of test cases generation.
+
+    Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+    Copyright © 2024, ZiLiang, all rights reserved.
+    Created: 24 October 2024
+======================================================================
+"""
+
+
+# ------------------------ Code --------------------------------------
+
+# normal import
+import json
+from typing import List, Tuple, Dict
+import random
+from pprint import pprint as ppp
+from collections import OrderedDict
+
+
+from constructor import retrievalFragments
+from constructor import retrievalFragments2Myself
+
+
+def main1():
+    """
+    the first main function for the construction of the retrieval.
+    """
+
+    # TARGET: test the effecacy of the CASES of self-contained choices
+    # that is, the =false choice= comes from the same paper from the
+    # correct answers.
+
+    # Step 1: Load papers from the SPIDERED FILE.
+
+    from_pth = "./recent_save_articles.json"
+    with open(from_pth,
+              'r', encoding='utf8') as f:
+        data = json.load(f, object_pairs_hook=OrderedDict)
+    papers = data["text"]
+    print(f"All paper Number: {len(papers)}")
+
+    cases=retrievalFragments2Myself(
+        papers4Q=papers,
+        save_path="test_cases_for_past_one_monthss_papers.json",
+        topk=3,
+        embed_method="tfidf",
+    )
+
+    print(cases[-1])
+
+
+
+
+if __name__=="__main__":
+    main1()
---
+++ data/INSTRUCTION.py
@@ -0,0 +1,13 @@
+"""
+======================================================================
+INSTRUCTION --- 
+
+    Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+    Copyright © 2024, ZiLiang, all rights reserved.
+    Created: 24 October 2024
+======================================================================
+"""
+
+
+INSTRUCTION="""Given a context, and four choice marked as A, B, C, and D, your need to select the correct text which is the next sequence of the context provided. You shouldn't provide any other information (like explanations of your choice) or re-say the choice in your Answer. Just four letters, A, B, C, or D.
+"""
