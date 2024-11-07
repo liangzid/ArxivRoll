aed89c6c01e802678d9110307e577235ea043813
liangzid <2273067585@qq.com>
2024-11-07 12:55:21 +0800

    try to fix the bug of encoding downloaded files. add the formatting of reranking.

---
+++ scp.py
@@ -0,0 +1,124 @@
+"""
+======================================================================
+SCP ---
+
+Functions of how we construct test cases.
+
+    Author: Zi Liang <zi1415926.liang@connect.polyu.hk>
+    Copyright © 2024, ZiLiang, all rights reserved.
+    Created:  7 November 2024
+======================================================================
+"""
+
+
+# ------------------------ Code --------------------------------------
+
+# normal import
+import json
+from typing import List, Tuple, Dict, Optional
+import random
+from pprint import pprint as ppp
+import numpy as np
+import math
+
+from post_process_paper_text import paper2fragments as p2f
+from utils import random_take_one
+from Vectorize import getEmbed
+from SearchBySomething import findSim
+from utils import constructTestCase
+from utils import constructSequencingTestCase
+
+
+def SCP(
+        paper: str, scp_type: str,
+        n_gram=1, minimal_char=80,
+) -> Optional[Dict[str, str], None]:
+    """
+    ------------------------------------------------------------------
+    core function to generate test cases from a paper string.
+    ------------------------------------------------------------------
+    paper: a string of the arxiv paper.
+    scp_type: s, c, or p.
+    ------------------------------------------------------------------
+    return: a selection dict like:
+    ```python
+        {
+            "context": context,
+            "A": choices[0],
+            "B": choices[1],
+            "C": choices[2],
+            "D": choices[3],
+            "label": text_label,
+        }
+    ```
+    ------------------------------------------------------------------
+    """
+    assert scp_type in ["s", "c", "p"]
+
+    # 1. split paper into fragments
+    query_frags = p2f(paper, n_gram, minimal_char)
+
+    if len(query_frags) <= 4:
+        return None
+
+    # 2. first select a fragment
+    idx, gram = selectFragment(query_frags)
+
+    # 3. then do SCP
+    testCase = None
+    if scp_type == "s":
+        num_rerank_parts = 3
+        split_symbol = ". "
+
+        # now split `num_rerank_parts` for the `gram`.
+        gram_sents = gram.split(split_symbol)
+        if len(gram_sents) < num_rerank_parts:
+            return None
+        constructed_grams = []
+        sent_num = math.floor(len(gram_sents)/num_rerank_parts)
+        for i_idx in range(num_rerank_parts-1):
+            constructed_grams.append(
+                split_symbol.join(
+                    gram_sents[sent_num*i_idx:sent_num*(i_idx+1)])
+            )
+        constructed_grams.append(
+            split_symbol.join(gram_sents[sent_num*(num_rerank_parts-1):])
+        )
+        assert len(constructed_grams) == num_rerank_parts
+        # then rerank it.
+        shuffled_idx = list(range(num_rerank_parts))
+        random.shuffle(shuffled_idx)
+
+        testCase = constructSequencingTestCase(
+            shuffled_idx,
+            constructed_grams,
+        )
+    elif scp_type == "c":
+        pass
+    elif scp_type == "p":
+        embed_method = "tfidf"
+        q_embeds = getEmbed(
+            query_frags,
+            embed_method)
+        sim_idxes = findSim(
+            idx, q_embeds, topk=4,
+        )
+        context = query_frags[idx-1]
+        true_ans = query_frags[idx]
+        false_anss = []
+        for fidx in sim_idxes:
+            if fidx == idx:
+                continue
+            false_cand = query_frags[fidx]
+            false_anss.append(false_cand)
+        testCase = constructTestCase(
+            context,
+            true_ans,
+            false_anss,
+            if_strucutured=True
+        )
+    return testCase
+
+
+def selectFragment(frags: List[str]):
+    return random_take_one(frags, no_first=True)
--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ Some easy-to-used tools.
 
 # normal import
 # import json
-# from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict
 import re
 import random
 # from pprint import pprint as ppp
@@ -32,7 +32,7 @@ def notMath(text: str):
         "subscript",
         "_", "=", "+", "*", "∘", "\\", "^", "|",
         "λ", "ρ⁢", "⁢∑", "ϑ", "𝑥", "Ω⁢",
-        ]
+    ]
 
     for sw in sen_words:
         # count = len(re.findall(sw, text))
@@ -129,3 +129,66 @@ def constructTestCase(
             "D": choices[3],
             "label": text_label,
         }
+
+
+def constructSequencingTestCase(
+        shuffled_idx: List[int],
+        grams: List[str],
+) -> Dict[str, str]:
+
+    idx_Letter_map = {
+        0: "A",
+        1: "B",
+        2: "C",
+        3: "D",
+        4: "E",
+        5: "F",
+    }
+
+    # serialize of the `Input`:
+    overall_text = ""
+    for i, idx in enumerate(shuffled_idx):
+        overall_text += f"**{idx_Letter_map[i]}: **" +\
+            f"{grams[idx]}"
+    correct_ans = ""
+    for idx in shuffled_idx:
+        correct_ans += idx_Letter_map[idx]
+
+    I = 10000
+    distractor1 = correct_ans
+    distractor2 = correct_ans
+    distractor3 = correct_ans
+    # random generate three distractors.
+    newshuffled_idx = list(range(len(shuffled_idx)))
+    i = 0
+    while distractor1 == correct_ans and i < I:
+        random.shuffle(newshuffled_idx)
+        distractor1 = "".join([idx_Letter_map[x] for x in newshuffled_idx])
+        i += 1
+    i = 0
+    while distractor2 == correct_ans and i < I:
+        random.shuffle(newshuffled_idx)
+        distractor2 = "".join([idx_Letter_map[x] for x in newshuffled_idx])
+        i += 1
+    i = 0
+    while distractor3 == correct_ans and i < I:
+        random.shuffle(newshuffled_idx)
+        distractor3 = "".join([idx_Letter_map[x] for x in newshuffled_idx])
+        i += 1
+
+    # finally shuffle the selections:
+    choices = [correct_ans, distractor1, distractor2, distractor3]
+    random.shuffle(choices)
+
+    label_idx = -1
+    for i, x in enumerate(choices):
+        if x == correct_ans:
+            label_idx = i
+    return {
+        "shuffled_text": overall_text,
+        "Selection 1": choices[0],
+        "Selection 2": choices[1],
+        "Selection 3": choices[2],
+        "Selection 4": choices[3],
+        "label": f"Selection {i+1}",
+    }
