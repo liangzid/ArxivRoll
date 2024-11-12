aff29e76f66b93d97e4f8a1ea79eb7a80e122882
liangzid <2273067585@qq.com>
2024-11-12 20:15:14 +0800

    update the code of dataset construction.

--- 1.run_vanilla_construct.py
+++ 1.run_vanilla_construct.py
@@ -14,6 +14,7 @@ Vanilla version of test cases generation.
 # ------------------------ Code --------------------------------------
 
 # normal import
+import os
 import json
 from typing import List, Tuple, Dict
 import random
@@ -49,21 +50,36 @@ def test_Latex():
     push2HF(testcase_path, name="robench_2024b-test-latex-P")
 
 
-def test_SCP_S():
-    # Step 1: Load papers from the SPIDERED FILE.
-    from_pth = "./recent_save_articles.json"
-    with open(from_pth,
-              'r', encoding='utf8') as f:
-        data = json.load(f, object_pairs_hook=OrderedDict)
-    papers = data["text"]
-    print(f"All paper Number: {len(papers)}")
+def test_SCP_S(pth=None, is_html=True,):
+    if pth is None:
+        # # Step 1: Load papers from the SPIDERED FILE.
+        # ### HTML version
+        # from_pth = "./recent_save_articles.json"
+        # with open(from_pth,
+        #           'r', encoding='utf8') as f:
+        #     data = json.load(f, object_pairs_hook=OrderedDict)
+        # papers = data["text"]
+        # print(f"All paper Number: {len(papers)}")
 
-    # from_pth = "./past_one_months_cache/overaltex.json"
-    # with open(from_pth,
-    #           'r', encoding='utf8') as f:
-    #     data = json.load(f, object_pairs_hook=OrderedDict)
-    # papers = data
-    # print(f"All paper Number: {len(papers)}")
+        from_pth = "./past_one_months_cache/overaltex.json"
+        with open(from_pth,
+                  'r', encoding='utf8') as f:
+            data = json.load(f, object_pairs_hook=OrderedDict)
+        papers = data
+        print(f"All paper Number: {len(papers)}")
+    else:
+        if is_html:
+            with open(pth,
+                      'r', encoding='utf8') as f:
+                data = json.load(f, object_pairs_hook=OrderedDict)
+            papers = data["text"]
+            print(f"All paper Number: {len(papers)}")
+        else:
+            with open(pth,
+                      'r', encoding='utf8') as f:
+                data = json.load(f, object_pairs_hook=OrderedDict)
+            papers = data
+            print(f"All paper Number: {len(papers)}")
 
     # # ---------------------------------------------------------------
     # cases = constructBenchmarksSCP(
@@ -140,7 +156,68 @@ def main_old():
     push2HF(testcase_path, name="robench_2024b-testII")
 
 
+def main():
+    directory = "./robench2024b_all/"
+    is_html = True
+    files = os.listdir(directory)
+    for f in files:
+        if f.endswith("jsonl"):
+            continue
+        pth = directory+f
+        if is_html:
+            with open(pth,
+                      'r', encoding='utf8') as f:
+                data = json.load(f, object_pairs_hook=OrderedDict)
+            papers = data["text"]
+            print(f"All paper Number: {len(papers)}")
+
+        save_path = f"{pth}SCP-s.jsonl"
+        constructBenchmarksSCP(
+            papers,
+            hf_style_save_path=save_path,
+            scp_type="s",
+            n_gram=2,
+            minimal_char=250,
+        )
+        newdatasetname = save_path.replace("./", "")\
+            .replace(".jsonl", "")\
+            .replace("/", "_").replace(".json", "")\
+            .replace("recent6months_html_", "")
+        push2HF(save_path, name=newdatasetname)
+
+        save_path = f"{pth}SCP-c.jsonl"
+        constructBenchmarksSCP(
+            papers,
+            hf_style_save_path=save_path,
+            scp_type="c",
+            n_gram=5,
+            minimal_char=400,
+        )
+        newdatasetname = save_path.replace("./", "")\
+            .replace(".jsonl", "")\
+            .replace("/", "_").replace(".json", "")\
+            .replace("recent6months_html_", "")
+        push2HF(save_path, name=newdatasetname)
+
+        save_path = f"{pth}SCP-p.jsonl"
+        constructBenchmarksSCP(
+            papers,
+            hf_style_save_path=save_path,
+            scp_type="p",
+            n_gram=1,
+            minimal_char=100,
+        )
+        newdatasetname = save_path.replace("./", "")\
+            .replace(".jsonl", "")\
+            .replace("/", "_").replace(".json", "")\
+            .replace("recent6months_html_", "")
+        push2HF(save_path, name=newdatasetname)
+
+        # break
+
+
 if __name__ == "__main__":
     # main_old()
     # test_Latex()
-    test_SCP_S()
+    # test_SCP_S()
+    main()
