e348996ca9c003ccc018d9d9ad8a64b645e859ab
liangzid <2273067585@qq.com>
2024-12-02 15:05:54 +0800

    update the code for evaluating the stability of generation.

--- eval_benchmark_stablity.py
+++ eval_benchmark_stablity.py
@@ -20,9 +20,115 @@ from typing import List, Tuple, Dict
 import random
 from pprint import pprint as ppp
 from collections import OrderedDict
+from tqdm import tqdm
+
+import torch
 
 from constructor import push2HF
 from constructor import constructBenchmarksSCP
+
+
+def intrisicInfer(
+        modelname,
+        scp_type="s",
+        max_length=2048,
+):
+    # 1. load model
+    from transformers import AutoModelForCausalLM, AutoTokenizer
+    from transformers import pipeline
+
+    model = AutoModelForCausalLM.from_pretrained(
+        modelname,
+        device_map="auto",
+        trust_remote_code=True,
+        torch_dtype=torch.bfloat16,
+    )
+    tokenizer = AutoTokenizer\
+        .from_pretrained(modelname)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+    text_gen = pipeline(
+        task="text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_length=max_length,
+        max_new_tokens=20,
+        do_sample=False,
+    )
+    print("Loading Done.")
+
+    # 2. load test set
+    # assert scp_type in ["s", "c", "p"]
+    assert scp_type in ["s",]
+    # time_ls=list(range(32))
+    time_ls = list(range(2))
+
+    overall_dict={}
+
+    for time in time_ls:
+        fpth = f"./multiTimeEvalBackup/Experiments_Time{time}_SCP-{scp_type}.jsonl"
+
+        datals = []
+        # from collections import OrderedDict
+        with open(fpth, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.endswith("\n"):
+                    line = line[:-1]
+                data = json.loads(line)
+                datals.append(data)
+        print("Now for generation.")
+        # Now for inference
+        for data in tqdm(datals, desc="Test Cases",):
+            hit=0.
+            if scp_type == "s":
+                shuffled_text = data["shuffled_text"]
+                A = data["A"]
+                B = data["B"]
+                C = data["C"]
+                D = data["D"]
+                label = data["label"]
+                inp = f"""## Instruction:\n Given a **shuffled text** composed of sentences A, B, and C, your task is to select the correct order from four available selections. Avoid providing any additional information (such as explanations of your choice) or restating the sentences in your answer. Simply provide your selection: Selection 1, Selection 2, Selection 3, or Selection 4.\n## Shuffled text:\n{shuffled_text}\n## Choice:\n**Selection 1** {A}\n**Selection 2** {B}\n**Selection 3** {C}\n**Selection 4** {D}\nAnswer:"""
+                output = text_gen(inp, num_return_sequences=1,
+                                  return_full_text=False,)
+                output = output[0]["generated_text"]
+                if label in output:
+                    hit+=1
+                print(output)
+
+            elif scp_type == "c":
+                text_with_holes = data["text_with_holes"]
+                text_candidates = data["text_candidates"]
+                A = data["A"]
+                B = data["B"]
+                C = data["C"]
+                D = data["D"]
+                label = data["label"]
+                inp = f"""## Instruction:\n Given a **masked paragraph** with three masked sentences marked as '<|MaskedSentence|>' and candidate sentences labeled A, B, and C, your task is to fill in the correct sentences to the masked positions by selecting the appropriate answers from four provided selections. Avoid providing any additional information (such as explanations of your choice) or restating the sentences in your answer. Simply provide your selection: Selection 1, Selection 2, Selection 3, or Selection 4.\n## Masked paragraph:\n{text_with_holes}\n## {text_candidates}\n ## Choice:\n**Selection 1** {A}\n**Selection 2** {B}\n**Selection 3** {C}\n**Selection 4** {D}\nAnswer:"""
+                output = text_gen(inp, num_return_sequences=1,
+                                  return_full_text=False,)
+                output = output[0]["generated_text"]
+                if label in output:
+                    hit+=1
+                print(output)
+            elif scp_type == "p":
+                context = data["context"]
+                A = data["A"]
+                B = data["B"]
+                C = data["C"]
+                D = data["D"]
+                label = data["label"]
+                inp = f"""## Instruction:\n Given a context, and four choices marked as A, B, C, and D, your task is to select the correct text which is the next sequence of the provided context. Avoid providing any additional information (such as explanations of your choice) or restating the choice in your answer. Simply provide one of the four letters: A, B, C, or D.\n## Context:\n{context}\n## Choice:\n**A** {A}\n**B** {B}\n**C** {C}\n**D** {D}\nAnswer:"""
+                output = text_gen(inp, num_return_sequences=1,
+                                  return_full_text=False,)
+                output = output[0]["generated_text"]
+                if label in output:
+                    hit+=1
+                print(output)
+        overall_dict[time]=hit/len(datals)
+
+    print(overall_dict)
+
 
 
 def multitimeGenerateBenches(
@@ -58,4 +164,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    intrisicInfer("meta-llama/Llama-3.1-8B-Instruct")
